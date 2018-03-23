3f25b9d83e68a3cf8fcdb943880fea3d5518e6eb
hieuvo <trunghieusgn@gmail.com>
2018-03-23 09:51:40 -0400

    simplify configurations

--- consumer-service/src/main/java/com/example/ConsumerApp.java
+++ consumer-service/src/main/java/com/example/ConsumerApp.java
@@ -2,18 +2,24 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ConsumerApp {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class, args);
     }
 }
 
-// todo log format string, String.format ('%s...')
-// todo RestTemplate uri pattern (//greetings-service/hi/{name})
-// todo git push client side load balance with Ribbon
-// todo how long after a service is down, registry server (Eureka Server) will get noticed?
 // todo Eureka Server clustering?
